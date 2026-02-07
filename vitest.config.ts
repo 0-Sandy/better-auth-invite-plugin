79969165eddb81db42d72854eefc0bac0012bac4
0-Sandy <github@sandy00.aleeas.com>
2026-02-07 16:27:45 -0300

    ci: add ci workflow and increase test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		testTimeout: 10000,
+		testTimeout: 15000,
 		coverage: {
 			exclude: [
 				...(configDefaults.coverage.exclude ?? []),
